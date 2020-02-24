a3117cc3350d4ba9eedce115b33db00f6e3b3c04
Karen Mitchell <karen1752000@yahoo.com>
2020-02-24 17:39:01 -0500

    Added a max and min leangth to username and password

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-//Show Input Error Message
+// Show Input Error Message
 function showError(input, message) {
   const formControl = input.parentElement;
   formControl.className = 'form-control error';
@@ -12,13 +12,13 @@ function showError(input, message) {
   small.innerText = message;
 }
 
-//Show Success Input
+// Show Success Input
 function showSuccess(input) {
   const formControl = input.parentElement;
   formControl.className = 'form-control success';
 }
 
-//Check Email is Valid
+// Check Email is Valid
 function isValidEmail(email) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
@@ -28,11 +28,30 @@ function isValidEmail(email) {
 function checkRequired(inputArr) {
   inputArr.forEach(function(input) {
     if (input.value.trim() === '') {
-      showError(input, 'is required');
+      showError(input, `${getFieldName(input)} is required`);
     } else {
       showSuccess(input);
     }
   });
+}
+
+// Check input length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} charaters`
+    );
+  } else if (input.value.length > max) {
+    showError(input, `${getFieldName(input)} must be at less ${max} charaters`);
+  } else {
+    showSuccess(input);
+  }
+}
+
+// Get Field Name
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
 //Event Listeners
@@ -40,4 +59,6 @@ form.addEventListener('submit', function(e) {
   e.preventDefault();
 
   checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
 });
